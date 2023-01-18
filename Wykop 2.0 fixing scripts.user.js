0c2421da2134059078bd3a8c74d6e786fa4c88f3
RzuF <rzuf22@gmail.com>
2023-01-18 23:11:40 +0100

    Changed to use MutationObserver to listen if any new content was loaded

--- Wykop 2.0 fixing scripts.user.js
+++ Wykop 2.0 fixing scripts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wykop 2.0 fixing scripts
 // @namespace    http://rzuf.pro
-// @version      1
+// @version      2.0
 // @description  Removes annoying click event from each entry that cause redirect by missclick
 // @author       RzuF
 // @match        https://wykop.pl/mikroblog*
@@ -14,15 +14,41 @@
 (function() {
     'use strict';
 
-    waitForKeyElements("article:not(.entry-block-skeleton)", stripEventFromArticle, true);
+    const strippedClass = "stripped-from-annoying-event";
 
-    function stripEventFromArticle(node) {
-        const article = node[0];
+    function stripEventFromArticle(article) {
         const cleanArticle = article.cloneNode(false);
+        cleanArticle.style = "cursor: initial;";
+        cleanArticle.classList.add(strippedClass);
         article.childNodes.forEach(childNode => {
             cleanArticle.appendChild(childNode);
         });
         article.parentNode.replaceChild(cleanArticle, article);
         console.log("Stripped article from annoying click event for: " + cleanArticle.parentElement.id);
     };
+
+    const config = { attributes: false, childList: true, subtree: true };
+    const callback = (mutationList, observer) => {
+        for (const mutation of mutationList) {
+            if (mutation.type === 'childList') {
+                observer.disconnect();
+                const articles = document.querySelectorAll("article:not(."+ strippedClass +")");
+                articles.forEach(article => {
+                    stripEventFromArticle(article);
+                });
+                observer.observe(document.body, config);
+            }
+        }
+    };
+    const observer = new MutationObserver(callback);
+    let observerLoaded = false;
+
+    waitForKeyElements("section.from-pagination-microblog>div.content", () => {
+        if(!observerLoaded) {
+            observerLoaded = true;
+            const contentDiv = document.querySelectorAll("section.from-pagination-microblog>div.content")[0];
+            console.log(contentDiv);
+            observer.observe(contentDiv, config);
+        }
+    }, true);
 })();
